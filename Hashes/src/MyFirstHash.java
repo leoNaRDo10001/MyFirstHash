73e9bceb9a63e0b09185b6dda2abd61a90074ab3
leoNaRDo10001 <65257072+leoNaRDo10001@users.noreply.github.com>
2022-12-21 22:34:45 +0100

    first commit

---
+++ Hashes/src/MyFirstHash.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+public class MyFirstHash {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> h = new HashSet<String>();
+		h.add("Lemur");
+		h.add("Orangutang");
+		h.add("Spider");
+		h.add("Gorilla");
+		
+		System.out.println(h.size());
+		
+		System.out.println(h);
+	}
+
+}
